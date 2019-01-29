01d62ca1417260c065961237e696e4eb2dc927ac
Gavin Mullally <mullg389@newschool.edu>
2019-01-29 09:02:30 -0500

    add content

---
+++ projects/time capsule/script.js
@@ -0,0 +1,69 @@
+var number1 = document.getElementById("1");
+number1.onclick = function(){
+    console.log("click1");
+    this.style.color="number";
+    document.getElementById("content").innerHTML ='<br><img src="1.jpg"></br>'
+ }   
+
+var number2= document.getElementById("2");
+number2.onclick = function(){
+    console.log("click2");
+    this.style.color="orange";
+    document.getElementById("content").innerHTML = '<br><img src="2.jpg"></br>'
+ }   
+
+var number3 = document.getElementById("3");
+number3.onclick = function(){
+    console.log("click3");
+    this.style.color="blue";
+    document.getElementById("content").innerHTML ='<br><img src="3.jpg"></br>'
+ }   
+
+var number4 = document.getElementById("4");
+number4.onclick = function(){
+    console.log("click4");
+    this.style.color="red";
+    document.getElementById("content").innerHTML ='<br><img src="4.jpg"></br>'
+ }  
+
+var number5 = document.getElementById("5");
+number5.onclick = function(){
+    console.log("click5");
+    this.style.color="orange";
+    document.getElementById("content").innerHTML ='<br><img src="5.jpg"></br>'
+ }   
+
+var number6 = document.getElementById("6");
+number6.onclick = function(){
+    console.log("click6");
+    this.style.color="blue";
+    document.getElementById("content").innerHTML ='<br><img src="6.jpg"></br>'
+ }   
+
+var number7 = document.getElementById("7");
+number7.onclick = function(){
+    console.log("click7");
+    this.style.color="red";
+    document.getElementById("content").innerHTML ='<br><img src="7.jpeg"></br>'
+ }  
+
+ var number8 = document.getElementById("8");
+number8.onclick = function(){
+    console.log("click8");
+    this.style.color="orange";
+    document.getElementById("content").innerHTML ='<br><img src="8.jpeg"></br>'
+ }  
+
+ var number9 = document.getElementById("9");
+number9.onclick = function(){
+    console.log("click9");
+    this.style.color="blue";
+    document.getElementById("content").innerHTML ='<br><img src="9.jpg"></br>'
+ }  
+
+ var number10 = document.getElementById("10");
+number10.onclick = function(){
+    console.log("click10");
+    this.style.color="red";
+    document.getElementById("content").innerHTML ='<br><img src="10.jpeg"></br>'
+ }  
