9182c6f3df9b665e6236636449a46b7a4bb86046
Gavin Mullally <mullg389@newschool.edu>
2019-03-06 19:44:32 -0500

    upload

---
+++ projects/javascriptexercise/script.js
@@ -0,0 +1,107 @@
+var firstName = 'Gavin';
+console.log ('My name is', firstName)
+
+var number = 20;
+console.log ('My number is', number)
+
+var number = '20';
+console.log ('My number is', number)
+
+var x = 0;
+console.log ('1>0') 
+
+var firstName = ['G','a','v','i','n'];
+console.log (firstName)
+
+var numbers = [firstName, number, 5];
+console.log (data)
+
+var lastName = 'Mullally';
+var fullName = (myName + lastName);
+console.log (fullName)
+
+var onehundred = 51 + 49;
+console.log (onehundred)
+
+var firstLetter = first[1];
+var lastLetter = first[first.length-2];
+
+console.log (firstnameLetter, lastnameLetter)
+
+var word = ['supercalifragilisticexpialidocious'];
+word.length;
+
+var firstLast = [firstName.length == lastName.length]
+console.log (firstLast)
+
+var students = 15;
+if (students  >17){
+	var allstudentsHere = 'All students are present';
+}
+else{
+	var notallpresent = 'Not all students are present';
+}
+{
+console.log (allPresent || notPresent)
+}
+for (var i = 0; i < firstName.length; i++){
+console.log (firstName)
+}
+
+var date = new Date;
+var time = date.andTime();
+
+if ( hour == 0 ) {
+  console.log("the time is evening");
+} else if ( time > 7 & time < 9.40 ) {
+  console.log("The time is between 7 and 9:40");
+} else{
+  console.log("It's not between 7 and 9:40");
+}
+
+var day = date.getDay();
+if (day == 1){
+	console.log('the second day is monday');
+} else if (day ==2){
+	console.log('the third day is tuesday');
+} else if (day ==3){
+	console.log('the fourth day is wednesday');
+} else if (day ==4){
+	console.log('the fifth day is thursday'); 
+} else if (day ==5){
+	console.log('the 6th day is friday'); 
+} else if (day ==6){
+	console.log(' saturday'); 
+} else if (day ==7){
+	console.log('the first day is sunday'); 
+}
+var foods = ['sushi','italian', 'thai'];
+console.log (foods)
+
+var animals = ['dogs','fish','birds'];
+console.log (animals)
+
+var places = ['Seattle','Nantucket', 'NYC'];
+console.log (places)
+
+var pokemon = ['Eevee','Celebi', 'Charizard'];
+console.log (pokemon)
+
+var favorite = [foods, animals, places, pokemon];
+console.log (favorite)
+
+var arrayOfArr= [favorite];
+for (var i = 0; i < favorites.length; i++) {
+  console.log (array)
+}
+
+var nested link = [foods, animals, places, pokemon];
+for (var i = 0; i < favorites.length; i++) {
+  console.log (nested)
+}
+
+
+
+
+
+
